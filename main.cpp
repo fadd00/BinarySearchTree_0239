0bde601e29d8d789adab72a5a4e40d43fe2b753f
fadd00 <andhikahutama9@gmail.com>
2024-06-13 15:29:50 +0700

    membuat template awal

--- main.cpp
+++ main.cpp
@@ -3,23 +3,6 @@
 using namespace std;
 
 
-class Node{
-public:
-    string info;
-    Node* leftchild;
-    Node* rightchild;
-
-    Node (string i, Node* l, Node* r){
-    info = i ;
-    leftchild = l ;
-    rightchild = r;
-    };
-};
-class BinaryTree{
-    public:
-    Node* ROOT;
-};
-
 int main(){
 
 }
