880d233878439504494468f582ab4f4537a75d57
fadd00 <andhikahutama9@gmail.com>
2024-06-13 15:31:28 +0700

    membuat class node dan menambahkan atribut publik

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 
+class Node{
+public:
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+    Node (string i, Node* l, Node* r){
+    info = i ;
+    leftchild = l ;
+    rightchild = r;
+    };
+};
+
+
 int main(){
 
 }
